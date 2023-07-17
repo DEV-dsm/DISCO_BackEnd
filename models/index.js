5fd62b0f71d080a48a59c6a68d08105c74d53c5f
오송주 <dhthdwn7920@gmail.com>
2023-07-17 22:04:18 +0900

    프로젝트 세팅

---
+++ models/index.js
@@ -0,0 +1,18 @@
+const Sequelize = require("sequelize");
+const env = process.env.NODE_ENV || "development";
+const config = require("../config/config");
+
+const db = {};
+
+const sequelize = new Sequelize({ ...config, Sync: false });
+
+db.sequelize = sequelize;
+db.Sequelize = sequelize;
+
+db.user = require("./user")(sequelize, Sequelize);
+db.post = require("./post")(sequelize, Sequelize);
+db.like = require("./like")(sequelize, Sequelize);
+db.comment = require("./comment")(sequelize, Sequelize);
+db.cocomment = require("./cocomment")(sequelize, Sequelize);
+
+module.exports = db;
