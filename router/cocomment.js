1768bdaae07f97fe87fb8b6319ebbea31c660cfa
오송주 <dhthdwn7920@gmail.com>
2023-08-04 17:32:31 +0900

    router오류 수정

--- router/cocomment.js
+++ router/cocomment.js
@@ -2,8 +2,8 @@ const router = require("express")();
 const cocomment = require("../controller/cocomment");
 
 router.post("/create", cocomment.createCocomment);
-router.get("/?commentID=", cocomment.getCocommentList);
-router.patch("/?commentID=", cocomment.updateCocomment);
-router.delete("/?commentID=", cocomment.deleteCocomment);
+router.get("/:commentID", cocomment.getCocommentList);
+router.patch("/:cocommentID", cocomment.updateCocomment);
+router.delete("/:cocommentID", cocomment.deleteCocomment);
 
 module.exports = router;
--- router/comment.js
+++ router/comment.js
@@ -2,8 +2,8 @@ const router = require("express")();
 const comment = require("../controller/comment");
 
 router.post("/create", comment.createComment);
-router.get("/?postID=", comment.getCommentList);
-router.patch("/?commentID=", comment.updateComment);
-router.delete("/?commentID=", comment.deleteComment);
+router.get("/:postID", comment.getCommentList);
+router.patch("/:commentID", comment.updateComment);
+router.delete("/:commentID", comment.deleteComment);
 
 module.exports = router;
