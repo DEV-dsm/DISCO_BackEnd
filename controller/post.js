f53857ff9c4283880a086b782424a0d02a57a125
오송주 <dhthdwn7920@gmail.com>
2023-09-01 16:48:58 +0900

    post & like controller

---
+++ controller/post.js
@@ -0,0 +1,55 @@
+const { Op } = require("sequelize");
+const post = require("../models");
+
+const searchPost = async (req, res) => {
+  const { keyword } = req.qurey;
+
+  try {
+    let postList = await post.findAll({
+      [Op.or]: [
+        { title: { [Op.like]: `%${keyword}%` } },
+        { body: { [Op.like]: `%${keyword}%` } },
+      ],
+    });
+
+    return res.status(200).json({
+      message: "요청에 성공했습니다",
+      postList,
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(400).json({
+      message: "요청에 실패했습니다",
+    });
+  }
+};
+
+const getPost = async (req, res) => {
+  const postID = req.params.postID;
+
+  try {
+    const thisPost = await post.findOne({
+      where: { postID },
+    });
+
+    if (!thisPost) {
+      return res.status(404).json({
+        message: "게시물 찾을수 없음",
+      });
+    }
+
+    return res.status(200).json({
+      message: "요청에 성공했습니다",
+      thisPost,
+    });
+  } catch (err) {
+    return res.status(400).json({
+      message: "요청에 실패했습니다.",
+    });
+  }
+};
+
+module.exports = {
+  searchPost,
+  getPost,
+};
