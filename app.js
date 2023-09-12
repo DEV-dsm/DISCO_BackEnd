e6af4fd1df662f5a94dc268a1dd1f607ddca2c44
오송주 <dhthdwn7920@gmail.com>
2023-09-12 18:21:32 +0900

    Add CORS

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ const app = require("express")();
 const router = require("./router/index");
 const bodyparser = require("body-parser");
 const { sequelize } = require("./models/index");
+const cors = require("cors");
 
 const port = 8080;
+
+let corsOptions = {
+  origin: "https://localhost:8080",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
