80764726677773be3ae7579b5e0f253b66352ab3
오송주 <dhthdwn7920@gmail.com>
2023-09-12 17:34:17 +0900

    add authMiddleware

--- router/post.js
+++ router/post.js
@@ -1,10 +1,11 @@
 const router = require("express")();
 const post = require("../controller/post");
 const like = require("../controller/like");
+const authMiddleware = require("../middleware/token");
 
 router.get("/:postID", post.getPost);
 router.get("/search", post.searchPost);
-router.post("/:postID/like", like.createLike);
-router.delete("/:postID/like", like.deleteLike);
+router.post("/:postID/like", authMiddleware, like.createLike);
+router.delete("/:postID/like", authMiddleware, like.deleteLike);
 
 module.exports = router;
