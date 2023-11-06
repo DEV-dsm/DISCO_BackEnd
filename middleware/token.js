37c3ef9e97990fc8e5ba598801490ceec17dec71
오송주 <dhthdwn7920@gmail.com>
2023-11-06 17:23:20 +0900

    token middleware 수정

--- middleware/token.js
+++ middleware/token.js
@@ -1,21 +1,42 @@
 const jwt = require("jsonwebtoken");
+const { user } = require("../models");
 
-function authenticationMiddleware(req, res, next) {
+async function authenticationMiddleware(req, res, next) {
   // 토큰 추출
-  const token = req.headers["Authorization"];
+  console.log(req.headers)
+  const token = req.headers.authorization;
 
-  if (!token) {
-    return res.status(401).json({ message: "인증 토큰이 없습니다." });
-  }
-
-  // 토큰 검증
-  jwt.verify(token, process.env.SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(401).json({ message: "토큰이 유효하지 않습니다." });
+  try {
+    if (!token) {
+      return res.status(401).json({ message: "인증 토큰이 없습니다." });
     }
-    req.decoded = decoded; // 사용자 정보를 요청 객체에 추가
-    next();
-  });
+
+    const thisUser = await user.findOne({
+      where: { token },
+    });
+
+    if (!thisUser) {
+      return res.status(401).json({
+        message: "토큰이 DB값과 일치하지 않습니다.",
+      });
+    }
+
+    // 토큰 검증
+    jwt.verify(token, process.env.SECRET, (err, decoded) => {
+      if (err) {
+        return res.status(401).json({
+          message: "토큰이 유효하지 않습니다.",
+        });
+      }
+      req.decoded = decoded; // 사용자 정보를 요청 객체에 추가
+      next();
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      message: "토큰 인증과정 중 서버에서 오류발생",
+    });
+  }
 }
 
 module.exports = authenticationMiddleware;
