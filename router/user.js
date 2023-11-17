0468be4cbde939ebb3e5f2f4a2bc2f905d82424e
오송주 <dhthdwn7920@gmail.com>
2023-11-17 15:04:21 +0900

    fix misspelling & user Error

--- router/user.js
+++ router/user.js
@@ -5,14 +5,9 @@ const token = require("../middleware/token");
 
 router.post("/login", user.login);
 router.post("/signup", user.signup);
-<<<<<<< Updated upstream
 router.get("/logout", token, user.logout);
 router.post("/delete-account", token, user.deleteAccount);
 router.get("/info", token, user.getUserInfo);
 router.patch("/update", authMiddleware, user.updateUser);
-=======
-router.post("/logout", tocken, user.logout);
-router.delete("/", tocken, user.deleteAccount);
->>>>>>> Stashed changes
 
 module.exports = router;
